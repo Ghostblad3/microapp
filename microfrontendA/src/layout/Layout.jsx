ff692e8dfc2098378c67ecfd32fb69ff14cc69d1
Ghostblad3 <str.geo.gr@gmail.com>
2026-02-21 18:45:35 +0200

    add example that breaks lazy loading

--- microfrontendA/src/layout/Layout.jsx
+++ microfrontendA/src/layout/Layout.jsx
@@ -1,8 +1,79 @@
 import { Menu } from '../components/Menu';
 import styles from './styles/Layout.module.css';
-import { MicrofrontendARouter } from '../router/MicrofrontendARouter';
+import { lazy, Suspense } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const BASE_PATH = '/microfrontendA';
+
+const First = lazy(() =>
+  import('../components/First').then((module) => ({
+    default: module.First,
+  }))
+);
+
+const Second = lazy(() =>
+  import('../components/Second').then((module) => ({
+    default: module.Second,
+  }))
+);
+
+const Third = lazy(() =>
+  import('../components/third/Third').then((module) => ({
+    default: module.Third,
+  }))
+);
+
+const NestedComponent = lazy(() =>
+  import('../components/third/NestedComponent').then((module) => ({
+    default: module.NestedComponent,
+  }))
+);
+
+const routes = [
+  {
+    path: `${BASE_PATH}/first`,
+    exact: true,
+    Component: First,
+  },
+  {
+    path: `${BASE_PATH}/second`,
+    exact: true,
+    Component: Second,
+  },
+  {
+    path: `${BASE_PATH}/third`,
+    Component: Third,
+    routes: [
+      {
+        path: `${BASE_PATH}/third/subpage`,
+        Component: NestedComponent,
+      },
+    ],
+  },
+];
+
+const MicrofrontendARouter = () => {
+  return (
+    <Switch>
+      {routes.map(({ path, exact, Component, routes }, i) => (
+        <Route
+          key={i}
+          path={path}
+          exact={exact}
+          render={() => (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Component routes={routes} />
+            </Suspense>
+          )}
+        />
+      ))}
+    </Switch>
+  );
+};
 
 const Layout = () => {
+  console.log('works');
+
   return (
     <div className={styles.inner}>
       <Menu />
